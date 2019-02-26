19a7e15466e5ab7976312244c7c56e0226cd02b1
mv3717 <marco.violet-vianello17@imperial.ac.uk>
2019-02-26 15:29:56 +0000

    added first test that fails - going to implement code for it to pass

--- src/Rover.java
+++ src/Rover.java
@@ -1,2 +1,26 @@
 public class Rover {
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    private int x;
+    private int y;
+
+    public Rover() {
+        x = 0;
+        y = 0;
+    }
 }
